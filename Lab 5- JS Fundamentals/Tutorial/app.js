59448e943d2eee3c3d738bf7b6b89483c063ef7a
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-15 14:08:39 +0300

    Lab 6

--- Lab 5- JS Fundamentals/Tutorial/app.js
+++ Lab 5- JS Fundamentals/Tutorial/app.js
@@ -21,27 +21,103 @@ const height = 2.0;
 
 // console.log(...names)
 
-const names = ["abdulla Khedr,", "Mustafa Ilmi," , "Mohamed Omar,"]
-console.log(names)
-//Add to the end
+// const names = ["abdulla Khedr,", "Mustafa Ilmi," , "Mohamed Omar,"]
+// console.log(names)
+// //Add to the end
+//
+// names.push("Ibrahim Ullah")
+// console.log("Push" , names)
+//
+// //Delete from the end
+// names.pop()
+// console.log("Pop" , names)
+//
+// //Add to the beginning
+// names.unshift("Ahmed Al-Hayder")
+// console.log("Unshif" , names)
+//
+// //Delete from the beginning
+// names.shift()
+// console.log("Shift" , names)
+//
+// names.sort()
+// console.log("Sorted Array" , names)
+//
+// for (const name of names) {
+//     console.log(name)
+// }
 
-names.push("Ibrahim Ullah")
-console.log("Push" , names)
 
-//Delete from the end
-names.pop()
-console.log("Pop" , names)
+//Arrow functions
+//Function
 
-//Add to the beginning
-names.unshift("Ahmed Al-Hayder")
-console.log("Unshif" , names)
+// let anArray = [1,2,3,4 , "Ali" , "Omar" , [1.1,1.2 , 'A'], {name:'ali'}]
+// console.log(anArray[6][1])
+//
+// let nums = [1,2,3,4,5,6]
+//
+// function display(numbers){
+//     for (const num of numbers) {
+//         console.log(num)
+//     }
+// }
+//
+// display(nums)
 
-//Delete from the beginning
-names.shift()
-console.log("Shift" , names)
+//
+// let add = function (x, y, callFunction) {
+//     let result = x + y;
+//     callFunction(x,y,result);
+// }
+//
+// let add =  (x, y, callFunction) => {
+//     let result = x + y;
+//     callFunction(x,y,result);
+// }
+//
+// add(3, 6, (x,y,result) => console.log('The result is ' , result))
+// add(3, 6, (x,y,result) => console.log('The score is ' , result))
+//
+// function multiply(x, y){
+//     return x*y;
+// }
 
-names.sort()
-console.log("Sorted Array" , names)
+let numbers = [1, 2, -3, -4, 5, 6, 7, -1]
+numbers.forEach((num) => console.log(num))
+let sum = numbers.reduce((acc, x) => acc + x)
+let max = numbers.reduce((acc, x) => acc > x ? acc : x)
+let min = numbers.reduce((acc, x) => acc < x ? acc : x)
+
+console.log(`sum  = ${sum} , max = ${max} , min = ${min}`)
+
+//filter
+let allPositive = numbers.filter(x => x < 0)
+console.log(`All Positve Numbers  , ${allPositive}`)
+
+let mappedElements = numbers.map(x => x)
+console.log(`mapped = ${mappedElements}`)
+
+//map
+
+//
+//
+// function addTwoNumbers(acc,x){
+//     acc = acc + x ;
+//     return acc
+// }
+// let acc=0;
+// for (let i = 0; i < numbers.length; i++) {
+//     acc = addTwoNumbers(acc , numbers[i])
+// }
+// console.log(acc)
+//
+//
+
+
+
+
+
+
 
 
 
---
+++ Lab 5- JS Fundamentals/Tutorial/app2.js
@@ -0,0 +1,82 @@
+//When you load a page
+
+//What is JS
+// JavaScript is a lightweight, interpreted programming language.
+// It is designed for creating network-centric applications.
+// It is complimentary to and integrated with Java.
+// JavaScript is very easy to implement because it is integrated with HTML. It is open and cross-platform.
+
+let x = 12
+let y = 12.5
+const z = 'text'
+
+x = 33
+
+console.log(y.toExponential(4))
+
+
+
+console.log(`x = ${++x} , y = ${typeof y} , z = ${typeof z}`)
+//Decision Making
+
+if (x > 20) console.log(`${x} is greater than 20`)
+else console.log(`${x} is not greater than 20`)
+
+//Loops
+for (let i = 0; i < 10; i++) {
+    console.log(i, '- itteration')
+}
+
+while (x > 0) {
+    console.log(x--)
+}
+do {
+    console.log(y--)
+} while (y > 0)
+
+
+//functions
+function display() {
+    console.log('Hello from function')
+}
+
+//Void
+function display() {
+    console.log('Hello from function')
+}
+
+display()
+
+//returns a value
+
+function add(x, y) {
+    return x + y
+}
+
+console.log('The sum = ', add(2, 4))
+
+//Arrays
+const people = ['Abdulahi', 'Mohamed', 'Hassen', 'Yusuf', 'Ahmed']
+
+for (const person of people) {
+    console.log(person)
+}
+
+//Methods
+
+let joinedValues = people.join('  ')
+console.log(joinedValues)
+
+people.sort()
+console.log(people)
+
+people.push('Added to the end')  //adds value to the end
+console.log('After push', people)
+let value = people.pop()  //removes values from the end
+console.log('After pop', people, value)
+
+people.unshift(`Added to the beginning`) // add value from the beginning
+console.log('After unshift', people)
+value = people.shift()   //remove to the beginning
+console.log('After shift', people, value)
+
