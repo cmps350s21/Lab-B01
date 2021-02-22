e730b0e54cbe7346e8855a8057bba0d46cc8d045
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-22 16:29:15 +0300

    updated

--- Lab 7-Client JS/Tutorial/Lab 7 - Base Code/js/index.js
+++ Lab 7-Client JS/Tutorial/Lab 7 - Base Code/js/index.js
@@ -1,16 +1,24 @@
-const baseURL ='http://universities.hipolabs.com/search?country'
+const baseURL = 'http://universities.hipolabs.com/search?country'
 const searchBar = document.querySelector('#search-bar')
-const universities = document.querySelector('#universities')
+const universitiesDD = document.querySelector('#universities')
 const website = document.querySelector('#website')
-
-function loadUniversities(universities) {
-
-    `<option value="website">the name</option>`
-}
 
 async function getUniversities() {
     let url = `${baseURL}=${searchBar.value}`
-    let data  = await fetch(url)  //server might take 30 second
+    let data = await fetch(url)  //server might take 30 second
     let universities = await data.json()  // long
     loadUniversities(universities)
 }
+
+function loadUniversities(universities) {
+    let uniOptions = universities
+        .map(uni => `<option value=${uni.web_pages[0]}>${uni.name}</option>`)
+        .join('')
+    universitiesDD.innerHTML = uniOptions
+    loadWebsite(universities[0].web_pages[0])
+
+}
+
+function loadWebsite(websiteUrl){website.src = websiteUrl}
+
+
