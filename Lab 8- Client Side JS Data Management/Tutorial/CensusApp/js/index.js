7801cf7d930b7c7f016881c3e6c2e0242e037cae
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-01 15:32:57 +0300

    update 3:32

--- Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
@@ -6,6 +6,7 @@
 const countryInputBox = document.querySelector('#country')
 const populationInputBox = document.querySelector('#population')
 const formElement = document.querySelector('#form')
+const countriesTable = document.querySelector('#countries')
 
 
 //Step 2 : when the button is clicked I want a function called addCensus
@@ -14,20 +15,54 @@ formElement.addEventListener('submit', addCensus)
 // Step 0 : create/open our database
 const db = new Localbase('census.db')
 
+//Step 0.1 :
+showCensusData() //open the database and load the collection documents
+
+async function showCensusData() {
+    const census = await db.collection('census').get()
+    const rows = census.map(c => censusToHTMLRaw(c))
+    countriesTable.innerHTML = `
+               <thead>
+                    <tr>
+                        <th>Country</th>
+                        <th>Population</th>
+                        <th>Action</th>
+                    </tr>
+               </thead>
+               ${ rows.join('')}
+        `
+}
+
+function censusToHTMLRaw(c) {
+    return `
+        <tr>
+            <td>${c.country}</td>
+            <td>${c.population}</td>
+            <td>
+                <i class="fa fa-pencil">Edit</i>
+                <i class="fa fa-trash">Delete</i>
+            </td>
+        </tr>
+    `
+}
+
 async function addCensus(event) {
     if (!formElement.checkValidity()) return
 
     //convert this form into a census object
     event.preventDefault();
     const newCensus = form2Object(formElement)
+    newCensus.id = Date.now()
 
     //to add this census to the indexDB database in a collection
-    //called census
-    //long running operation
+    //called census //long running operation
     let successMessage = await db.collection('census').add(newCensus)
-    console.log(successMessage)
 
+    //we call again the show
+    const newCensusRow = censusToHTMLRaw(newCensus)
+    countriesTable.innerHTML += newCensusRow
 }
+
 function form2Object(formElement) {
     const formData = new FormData(formElement);
     const data = {}
