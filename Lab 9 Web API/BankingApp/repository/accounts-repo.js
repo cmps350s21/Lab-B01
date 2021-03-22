bb3a8b28d9c74c0df2c6b94b90c0ee7f37d74dce
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-22 17:02:59 +0300

    final code with error to writing into file

---
+++ Lab 9 Web API/BankingApp/repository/accounts-repo.js
@@ -0,0 +1,45 @@
+import fs from 'fs-extra'
+const filePath = '../data/accounts.json'
+
+class AccountsRepo{
+
+    async getAccounts({type}) {
+        const accounts = await fs.readJson(filePath)
+        if(type=='All')
+            return accounts
+        else if(type =='Current' || type == 'Saving')
+            return  accounts.filter(acc => acc.acctType == type)
+    }
+
+    async addAccount(account) {
+        const accounts = await fs.readJson(filePath)
+        accounts.push(account)
+        return await fs.writeJson(filePath , accounts)
+
+    }
+
+    async updateAccount(account) {
+        const accounts = await fs.readJson(filePath)
+        const index = accounts.findIndex(acc => acc.accountNo == account.accountNo)
+        if (index > 0) {
+            accounts[index] = account
+            return await fs.writeJson(filePath, accounts)
+        }
+        return null
+    }
+    async getAccount(accNo) {
+        const accounts = await fs.readJson(filePath)
+        const account = accounts.find(acc => acc.accountNo == accNo)
+        return account
+    }
+
+    async deleteAccount(accNo) {
+
+        const accounts = await fs.readJson(filePath)
+        const filteredAccounts = accounts.filter(acc => acc.accountNo != accNo)
+        console.log(filteredAccounts)
+        return await fs.writeJson(filePath, filteredAccounts)
+    }
+}
+
+export default AccountsRepo
--- Lab 9 Web API/BankingApp/service/accounts-services.js
+++ Lab 9 Web API/BankingApp/service/accounts-services.js
@@ -1,50 +1,33 @@
-let accounts = [
-    {
-        "accountNo": 1101,
-        "acctType": "Current",
-        "balance": 500
-    },
-    {
-        "accountNo": 2002,
-        "acctType": "Saving",
-        "balance": 8000,
-        "minimumBalance": 1000
-    }
-]
+import AccountsRepo from "../repository/accounts-repo.js";
+const accountRepo = new AccountsRepo()
+
 class AccountsServices {
     async getAccounts(req, res) {
-        const query = req.query
-        const filteredAccounts = accounts.filter(acc => acc.acctType == query.type)
-        res.json(filteredAccounts)
+        const accounts = await accountRepo.getAccounts(req.query)
+        res.status(200).json(accounts)
     }
 
     async addAccount(req, res) {
         const account = req.body
-        if (account)
-            accounts.push(account)
-        res.send('We are working on your request')
+        await accountRepo.addAccount(account)
+        res.status(201).send('successfully added your account')
     }
 
     async updateAccount(req, res) {
         const account = req.body
-        const index = accounts.findIndex(acc => acc.accountNo == account.accountNo)
-        if (index > 0) {
-            accounts[index] = account
-            res.send(`updated account ${JSON.stringify(account)}`)
-        } else {
-            res.send(`unable to update the account you are looking for`)
-        }
+        await accountRepo.updateAccount(account)
+        res.send('successfully updated')
     }
 
     async getAccount(req, res) {
         const accNo = req.params.acctNo
-        const account = accounts.find(acc => acc.accountNo == accNo)
+        const account  = await accountRepo.getAccount(accNo)
         res.json(account)
     }
 
     async deleteAccount(req, res) {
         const accNo = req.params.acctNo
-        accounts = accounts.filter(acc => acc.accountNo != accNo)
+        await accountRepo.deleteAccount(accNo)
         res.send(`successfully deleted the account with account no ${accNo}`)
 
     }
