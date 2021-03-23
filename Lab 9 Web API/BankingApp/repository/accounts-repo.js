e5a77cf5a637a11b26afbef4779b599f45e44496
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-23 12:03:29 +0300

    Update accounts-repo.js

--- Lab 9 Web API/BankingApp/repository/accounts-repo.js
+++ Lab 9 Web API/BankingApp/repository/accounts-repo.js
@@ -1,21 +1,22 @@
 import fs from 'fs-extra'
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
+
 const url = new URL('../data/accounts.json', import.meta.url);
 const filePath = fileURLToPath(url);
 
-class AccountsRepo{
+class AccountsRepo {
     async getAccounts({type}) {
         const accounts = await fs.readJson(filePath)
-        if(type=='All')
+        if (type == 'All')
             return accounts
-        else if(type =='Current' || type == 'Saving')
-            return  accounts.filter(acc => acc.acctType == type)
+        else if (type == 'Current' || type == 'Saving')
+            return accounts.filter(acc => acc.acctType == type)
     }
 
     async addAccount(account) {
         const accounts = await fs.readJson(filePath)
         accounts.push(account)
-        return await fs.writeJson(filePath , accounts)
+        return await fs.writeJson(filePath, accounts)
     }
 
     async updateAccount(account) {
@@ -27,6 +28,7 @@ class AccountsRepo{
         }
         return null
     }
+
     async getAccount(accNo) {
         const accounts = await fs.readJson(filePath)
         const account = accounts.find(acc => acc.accountNo == accNo)
@@ -36,7 +38,6 @@ class AccountsRepo{
     async deleteAccount(accNo) {
         const accounts = await fs.readJson(filePath)
         const filteredAccounts = accounts.filter(acc => acc.accountNo != accNo)
-        console.log(filteredAccounts)
         return await fs.writeJson(filePath, filteredAccounts)
     }
 }
